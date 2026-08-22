bdf67f6146da8d81beb6af3b4a17f894bee995a0
jackersthecoltsfan <jackersthecoltsfan@gmail.com>
2026-08-22 17:10:36 -0400

    Add files via upload

---
+++ LifeRoute/LifeRouteApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct LifeRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}

86bca59bc8e529a18a61ae261db37745375166da
jackersthecoltsfan <jackersthecoltsfan@gmail.com>
2026-08-23 16:57:39 -0400

    Use system host background for customizable UI

--- LifeRoute/ContentView.swift
+++ LifeRoute/ContentView.swift
@@ -2,13 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(red: 0.03, green: 0.07, blue: 0.12)
-                .ignoresSafeArea()
-
-            LifeRouteWebView()
-                .ignoresSafeArea(edges: .bottom)
-        }
+        LifeRouteWebView()
+            .background(Color(uiColor: .systemBackground))
+            .ignoresSafeArea(edges: .bottom)
     }
 }
 
